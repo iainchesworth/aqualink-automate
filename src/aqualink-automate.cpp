0a9f7cc4067854e0389c87c32a9917a4c12cded3
Iain Chesworth <iain.chesworth@gmail.com>
2022-12-05 14:20:55 +1100

    Add project files.

---
+++ src/aqualink-automate.cpp
@@ -0,0 +1,12 @@
+﻿// aqualink-automate.cpp : Defines the entry point for the application.
+//
+
+#include "aqualink-automate.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
