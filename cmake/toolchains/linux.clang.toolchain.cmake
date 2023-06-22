1446352eb37fb91ac8f779bfef6d2f8ddce08b13
Iain Chesworth <iain.chesworth@gmail.com>
2023-06-22 21:36:11 +1000

    Removing toolchain detection/forcing to clang-17; leave it to the environment to specify the compiler (although double-check it in the toolchain).

--- cmake/toolchains/linux.clang.toolchain.cmake
+++ cmake/toolchains/linux.clang.toolchain.cmake
@@ -6,24 +6,11 @@
 #------------------------------------------------------------------------------
 
 # Detect compiler
-if(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
-  # If GCC, check the version
-  if(CMAKE_CXX_COMPILER_VERSION VERSION_LESS "17.0.0")
-    message(FATAL_ERROR "Clang version must be at least 17.0.0")
-  else()
+if(CMAKE_CXX_COMPILER_ID NOT STREQUAL "Clang")
     message(FATAL_ERROR "Clang is not the specifed compiler...expected a Clang compiler")
-  endif()
 endif()
 
 # Toolchain settings go here (if required)
-
-if(NOT DEFINED CMAKE_C_COMPILER)
-set(CMAKE_C_COMPILER "clang-17")
-endif(NOT DEFINED CMAKE_C_COMPILER)
-
-if(NOT DEFINED CMAKE_CXX_COMPILER)
-set(CMAKE_CXX_COMPILER "clang++-17")
-endif(NOT DEFINED CMAKE_CXX_COMPILER)
 
 #------------------------------------------------------------------------------
 #
