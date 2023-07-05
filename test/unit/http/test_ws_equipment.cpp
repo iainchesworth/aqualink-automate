da51d53bb4c2fe78071807fe30416bc318aff1e7
Iain Chesworth <iain.chesworth@gmail.com>
2023-07-05 13:38:16 +1000

    Abstract away the details of the HTTP server and websocket client for the websocket tests.

--- test/unit/http/test_ws_equipment.cpp
+++ test/unit/http/test_ws_equipment.cpp
@@ -20,7 +20,85 @@
 
 using namespace AqualinkAutomate;
 
-BOOST_FIXTURE_TEST_SUITE(WebsocketRoutes_WsEquipment, Test::OneTouchDevice)
+class Test_OneTouchDevicePlusHttpServer : public Test::OneTouchDevice
+{
+	const std::string LISTEN_ADDR{ "127.0.0.1" };
+	const uint16_t LISTEN_PORT{ 80 };
+
+public:
+	Test_OneTouchDevicePlusHttpServer() : 
+		Test::OneTouchDevice(),
+		m_HTTPServer(),
+		m_WS_Equipment(m_HTTPServer, DataHub()),
+		m_HTTPServerThread()
+	{
+		m_HTTPServer
+			.bindaddr(LISTEN_ADDR)
+			.port(LISTEN_PORT)
+			.loglevel(crow::LogLevel::Critical);
+	}
+
+	~Test_OneTouchDevicePlusHttpServer()
+	{
+		if(ws.is_open())
+		{
+			ws.close(boost::beast::websocket::close_code::normal);
+		}
+
+		m_HTTPServer.stop();
+
+		if (m_HTTPServerThread.joinable())
+		{
+			m_HTTPServerThread.join();
+		}		
+	}
+
+public:
+	void ConfigureWebSocketServer()
+	{
+		BOOST_REQUIRE_NO_THROW(m_HTTPServer.validate());
+
+		m_HTTPServerThread = std::thread([&]() -> void
+			{
+				m_HTTPServer.run();
+			}
+		);
+
+		m_HTTPServer.wait_for_server_start();
+	}
+
+	void ConfigureWebSocketClient(const std::string& ws_route)
+	{
+		auto const results = resolver.resolve(LISTEN_ADDR, std::to_string(LISTEN_PORT));
+		boost::asio::connect(ws.next_layer(), results.begin(), results.end());
+
+		ws.set_option(boost::beast::websocket::stream_base::decorator(
+			[](boost::beast::websocket::request_type& req)
+			{
+				req.set(boost::beast::http::field::user_agent, std::string(BOOST_BEAST_VERSION_STRING) + " aqualink-automate-websocket-client");
+			}));
+
+		ws.handshake(LISTEN_ADDR, "/ws/equipment");
+	}
+
+public:
+	void ReadFromWebSocket_Blocking(boost::beast::flat_buffer& buffer)
+	{
+		ws.read(buffer);
+	}
+
+private:
+	crow::SimpleApp m_HTTPServer;
+	HTTP::WebSocket_Equipment m_WS_Equipment;
+	std::thread m_HTTPServerThread;
+
+private:
+	boost::asio::io_context ioc;
+	boost::asio::ip::tcp::resolver resolver{ioc};
+	boost::beast::websocket::stream<boost::asio::ip::tcp::socket> ws{ioc};
+};
+
+BOOST_FIXTURE_TEST_SUITE(WebsocketRoutes_WsEquipment, Test_OneTouchDevicePlusHttpServer)
 
 BOOST_AUTO_TEST_CASE(WebSocket_ChemistryEventConversion)
 {
@@ -123,38 +201,17 @@ BOOST_AUTO_TEST_CASE(WebSocket_TemperatureEventConversion)
 
 BOOST_AUTO_TEST_CASE(WebSocket_PublishChemistryUpdate)
 {
-	crow::SimpleApp http_server;
-	http_server.loglevel(crow::LogLevel::Critical);
-	HTTP::WebSocket_Equipment websocket_je(http_server, DataHub());
-	BOOST_REQUIRE_NO_THROW(http_server.validate());
-	auto http_server_tracker = http_server.run_async();
+	boost::beast::flat_buffer buffer;
+
+	ConfigureWebSocketServer();
+	ConfigureWebSocketClient("/ws/equipment");
 
 	{
-		auto const host = "localhost";
-		auto const port = "80";
-
-		boost::asio::io_context ioc;
-		boost::asio::ip::tcp::resolver resolver{ioc};
-		boost::beast::websocket::stream<boost::asio::ip::tcp::socket> ws{ioc};
-
-		auto const results = resolver.resolve(host, port);
-		boost::asio::connect(ws.next_layer(), results.begin(), results.end());
-		
-		ws.set_option(boost::beast::websocket::stream_base::decorator(
-			[](boost::beast::websocket::request_type& req)
-			{
-				req.set(boost::beast::http::field::user_agent, std::string(BOOST_BEAST_VERSION_STRING) + " aqualink-automate-websocket-client");
-			}));
-		
-		ws.handshake(host, "/ws/equipment");
-
 		// Send the message here....
 		DataHub().ORP(650);
 		DataHub().pH(7.5f);
 
-		boost::beast::flat_buffer buffer;
-		ws.read(buffer);
-
+		ReadFromWebSocket_Blocking(buffer);
 		{
 			auto req = buffer.data();
 			auto wse_json = nlohmann::json::parse(boost::asio::buffers_begin(req), boost::asio::buffers_end(req));
@@ -164,51 +221,39 @@ BOOST_AUTO_TEST_CASE(WebSocket_PublishChemistryUpdate)
 			BOOST_REQUIRE(wse_json.contains("payload"));
 			BOOST_REQUIRE(wse_json["payload"].contains("orp"));
 			BOOST_CHECK_EQUAL(650, wse_json["payload"]["orp"]);
+
+			buffer.clear();
+		}
+
+		ReadFromWebSocket_Blocking(buffer);
+		{
+			auto req = buffer.data();
+			auto wse_json = nlohmann::json::parse(boost::asio::buffers_begin(req), boost::asio::buffers_end(req));
+
+			BOOST_REQUIRE(wse_json.contains("type"));
+			BOOST_CHECK_EQUAL("ChemistryUpdate", wse_json["type"]);
+			BOOST_REQUIRE(wse_json.contains("payload"));
 			BOOST_REQUIRE(wse_json["payload"].contains("ph"));
 			BOOST_CHECK_EQUAL(7.5, wse_json["payload"]["ph"]);
+
+			buffer.clear();
 		}
-		
-		ws.close(boost::beast::websocket::close_code::normal);
 	}
-
-	http_server.stop();
-
 }
 
 BOOST_AUTO_TEST_CASE(WebSocket_PublishTemperatureUpdate)
 {
-	crow::SimpleApp http_server;
-	http_server.loglevel(crow::LogLevel::Critical);
-	HTTP::WebSocket_Equipment websocket_je(http_server, DataHub());
-	BOOST_REQUIRE_NO_THROW(http_server.validate());
-	auto http_server_tracker = http_server.run_async();
+	boost::beast::flat_buffer buffer;
+
+	ConfigureWebSocketServer();
+	ConfigureWebSocketClient("/ws/equipment");
 
 	{
-		auto const host = "localhost";
-		auto const port = "80";
-
-		boost::asio::io_context ioc;
-		boost::asio::ip::tcp::resolver resolver{ioc};
-		boost::beast::websocket::stream<boost::asio::ip::tcp::socket> ws{ioc};
-
-		auto const results = resolver.resolve(host, port);
-		boost::asio::connect(ws.next_layer(), results.begin(), results.end());
-
-		ws.set_option(boost::beast::websocket::stream_base::decorator(
-			[](boost::beast::websocket::request_type& req)
-			{
-				req.set(boost::beast::http::field::user_agent, std::string(BOOST_BEAST_VERSION_STRING) + " aqualink-automate-websocket-client");
-			}));
-
-		ws.handshake(host, "/ws/equipment");
-
 		// Send the message here....
 		auto pool_temp = Utility::Temperature("Pool        38`C");
 		DataHub().PoolTemp(pool_temp);
 
-		boost::beast::flat_buffer buffer;
-		ws.read(buffer);
-
+		ReadFromWebSocket_Blocking(buffer);
 		{
 			auto req = buffer.data();
 			auto wse_json = nlohmann::json::parse(boost::asio::buffers_begin(req), boost::asio::buffers_end(req));
@@ -218,13 +263,10 @@ BOOST_AUTO_TEST_CASE(WebSocket_PublishTemperatureUpdate)
 			BOOST_REQUIRE(wse_json.contains("payload"));
 			BOOST_REQUIRE(wse_json["payload"].contains("pool_temp"));
 			BOOST_CHECK_EQUAL("38\u00B0C", wse_json["payload"]["pool_temp"]);
+
+			buffer.clear();
 		}
-
-		ws.close(boost::beast::websocket::close_code::normal);
 	}
-
-	http_server.stop();
-
 }
 
 BOOST_AUTO_TEST_SUITE_END()
