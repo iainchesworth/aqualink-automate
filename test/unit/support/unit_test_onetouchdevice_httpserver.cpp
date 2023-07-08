5251aa2b32592960ed9b8232606696a2b3d5b7cb
Iain Chesworth <iain.chesworth@gmail.com>
2023-07-08 20:54:57 +1000

    Add a timeout to check for blocking HTTP API calls (due to a test failure or other reason).  This will fail tests if it occurs.

--- test/unit/support/unit_test_onetouchdevice_httpserver.cpp
+++ test/unit/support/unit_test_onetouchdevice_httpserver.cpp
@@ -1,3 +1,5 @@
+#include <boost/test/unit_test.hpp>
+
 #include <boost/asio/buffers_iterator.hpp>
 #include <boost/asio/connect.hpp>
 #include <boost/beast/core.hpp>
@@ -14,9 +16,13 @@ namespace AqualinkAutomate::Test
 		Test::OneTouchDevice(),
 		m_HTTPServer(1),
 		m_API_Equipment(m_HTTPServer, DataHub(), StatisticsHub()),
-		m_WS_Equipment(m_HTTPServer, DataHub())
+		m_WS_Equipment(m_HTTPServer, DataHub()),
+		m_HTTPServerThread()
 	{
 		m_HTTPServer.listen(LISTEN_ADDR, LISTEN_PORT);
+		
+		m_HTTPServer.enable_timeout(false);			// Disable keep-alives.
+		m_HTTPServer.enable_response_time(true);
 	}
 
 	Test_OneTouchDevicePlusHttpServer::~Test_OneTouchDevicePlusHttpServer()
@@ -25,37 +31,31 @@ namespace AqualinkAutomate::Test
 
 		http_stream.socket().shutdown(boost::asio::ip::tcp::socket::shutdown_both, ec);
 
-		if (ws_thread.joinable())
+		if (m_HTTPServerThread.joinable())
 		{
 			m_HTTPServer.get_io_service().stop();
-			ws_thread.join();
+			m_HTTPServerThread.join();
 		}
 	}
 
 	void Test_OneTouchDevicePlusHttpServer::StartHttpServer()
 	{
-		m_HTTPServer.enable_timeout(false);			// Disable keep-alives.
-		m_HTTPServer.enable_response_time(true);
-	}
-
-	void Test_OneTouchDevicePlusHttpServer::StartHttpClient()
-	{
-		auto const results = resolver.resolve(LISTEN_ADDR, LISTEN_PORT);
-
-		http_stream.connect(results);
-
-		m_HTTPServer.get_io_service().poll_one();
-	}
-
-	void Test_OneTouchDevicePlusHttpServer::StartWebSocketClient(const std::string& ws_route)
-	{
-		ws_thread = std::thread(
+		m_HTTPServerThread = std::thread(
 			[this]() -> void
 			{
 				m_HTTPServer.run();
 			}
 		);
+	}
 
+	void Test_OneTouchDevicePlusHttpServer::StartHttpClient()
+	{
+		auto const results = resolver.resolve(LISTEN_ADDR, LISTEN_PORT);
+		http_stream.connect(results);
+	}
+
+	void Test_OneTouchDevicePlusHttpServer::StartWebSocketClient(const std::string& ws_route)
+	{
 		auto const results = resolver.resolve(LISTEN_ADDR, LISTEN_PORT);
 		boost::asio::connect(ws_stream.next_layer(), results.begin(), results.end());
 
@@ -78,19 +78,45 @@ namespace AqualinkAutomate::Test
 		req.set(boost::beast::http::field::host, LISTEN_ADDR);
 		req.set(boost::beast::http::field::user_agent, std::string(BOOST_BEAST_VERSION_STRING) + " aqualink-automate-api-client");				
 
-		boost::beast::http::write(http_stream, req);
-
-		std::future<void> result_future = std::async(std::launch::async,
-			[this]() -> void
+		boost::beast::http::async_write(http_stream.socket(), req,
+			[this, &buffer, &res](const boost::system::error_code& ec, std::size_t bytes_transferred) -> void
 			{
-				m_HTTPServer.get_io_service().run_one();
-				m_HTTPServer.get_io_service().run_one();
+				if (ec)
+				{
+					BOOST_ERROR("Error occurred while performing GET request for HTTP server method test");
+				}
+				else
+				{
+					boost::beast::http::async_read(http_stream, buffer, res,
+						[this](const boost::system::error_code& ec, std::size_t bytes_transferred) -> void
+						{
+							m_ReadTimer.cancel_one();
+
+							ioc.stop();
+							ioc.reset();
+						}
+					);
+				}
 			}
 		);
 
-		boost::beast::http::read(http_stream, buffer, res);
+		m_ReadTimer.async_wait(
+			[this](const boost::system::error_code& ec) ->void
+			{
+				if (boost::asio::error::operation_aborted == ec.value())
+				{
+					// Everything is fine, the timer was cancelled so it's all good.
+				}
+				else
+				{
+					ioc.stop();
 
-		result_future.get();
+					BOOST_ERROR("Timeout while waiting for response from HTTP server during test");
+				}
+			}
+		);
+
+		ioc.run();
 	}
 
 	void Test_OneTouchDevicePlusHttpServer::ReadFromWebSocket_Blocking(boost::beast::flat_buffer& buffer)
