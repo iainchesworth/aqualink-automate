486bae088928498983b08201830107f7194f399f
Iain Chesworth <iain.chesworth@gmail.com>
2023-08-24 23:08:40 +1000

    Fix missing hubs in the unit test hub locator injector (as the equipment hub is leveraged by the websocket_equipment).

--- test/unit/support/unit_test_hublocatorinjector.cpp
+++ test/unit/support/unit_test_hublocatorinjector.cpp
@@ -1,6 +1,8 @@
 #include <memory>
 
 #include "kernel/data_hub.h"
+#include "kernel/equipment_hub.h"
+#include "kernel/preferences_hub.h"
 #include "kernel/statistics_hub.h"
 
 #include "support/unit_test_hublocatorinjector.h"
@@ -11,6 +13,8 @@ namespace AqualinkAutomate::Test
 	HubLocatorInjector::HubLocatorInjector()
 	{
 		Register(std::make_shared<Kernel::DataHub>());
+		Register(std::make_shared<Kernel::EquipmentHub>());
+		Register(std::make_shared<Kernel::PreferencesHub>());
 		Register(std::make_shared<Kernel::StatisticsHub>());
 	}
 
