38d62089400e21f8d5d5032ba4abbf4bfbed9f96
Iain Chesworth <iain.chesworth@gmail.com>
2023-10-16 13:08:05 +1100

    Restrict default HTTPS connections to be TLS or better i.e. no support for SSL v.anything.

--- src/aqualink-automate.cpp
+++ src/aqualink-automate.cpp
@@ -233,7 +233,7 @@ int main(int argc, char* argv[])
 
 		HTTP::Routing::StaticHandler(HTTP::StaticFileHandler("/", settings.web.doc_root));
 
-		boost::asio::ssl::context ssl_context(boost::asio::ssl::context::sslv23);
+		boost::asio::ssl::context ssl_context(boost::asio::ssl::context::tls_server);
 
 		if (!settings.web.http_server_is_insecure)
 		{
@@ -246,17 +246,6 @@ int main(int argc, char* argv[])
 				boost::asio::ssl::context::single_dh_use |			// Always create a new key using the tmp_dh parameters
 				static_cast<long>(SSL_OP_CIPHER_SERVER_PREFERENCE)
 			);
-
-			const std::string cipher_list
-			{
-				"TLS_AES_128_GCM_SHA256:"
-				"TLS_AES_256_GCM_SHA384:"
-				"TLS_CHACHA20_POLY1305_SHA256:"
-				"ECDHE-RSA-AES128-GCM-SHA256:"
-				"ECDHE-RSA-AES256-GCM-SHA384"
-			};
-
-			SSL_CTX_set_cipher_list(ssl_context.native_handle(), cipher_list.c_str());
 
 			Certificates::LoadSslCertificates(settings.web, ssl_context);
 		}
