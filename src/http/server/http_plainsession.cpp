f72d69e1bd26009d2c2e47f67b53a5ce44ba2727
Iain Chesworth <iain.chesworth@gmail.com>
2024-01-17 17:18:18 +1100

    Initial setup of unit test structure including changes to HTTP_PlainSession to support testability.

--- src/http/server/http_plainsession.cpp
+++ src/http/server/http_plainsession.cpp
@@ -1,45 +1,9 @@
-#include <format>
-
-#include <boost/system/error_code.hpp>
-
 #include "http/server/http_plainsession.h"
-#include "logging/logging.h"
-
-using namespace AqualinkAutomate::Logging;
 
 namespace AqualinkAutomate::HTTP
 {
 
-	HTTP_PlainSession::HTTP_PlainSession(boost::beast::tcp_stream&& stream, boost::beast::flat_buffer&& buffer) :
-		HTTP_Session<HTTP_PlainSession>(std::move(buffer)),
-		m_Stream(std::move(stream))
-	{
-	}
-
-	void HTTP_PlainSession::Run()
-	{
-		DoRead();
-	}
-
-	boost::beast::tcp_stream& HTTP_PlainSession::Stream()
-	{
-		return m_Stream;
-	}
-
-	boost::beast::tcp_stream HTTP_PlainSession::ReleaseStream()
-	{
-		return std::move(m_Stream);
-	}
-
-	void HTTP_PlainSession::DoEOF()
-	{
-		boost::system::error_code ec;
-
-		if (m_Stream.socket().shutdown(boost::asio::ip::tcp::socket::shutdown_send, ec); ec)
-		{
-			LogTrace(Channel::Web, std::format("Failed to shutdown tcp socket; error was -> {}", ec.message()));
-		}
-	}
+	// NOTHING HERE
 
 }
 // namespace AqualinkAutomate::HTTP
